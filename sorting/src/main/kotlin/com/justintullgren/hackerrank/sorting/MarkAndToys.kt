be303b819f3c0179d83ad68e8f1ee552564b2a8d
Justin Tullgren <justin.tullgren@gmail.com>
2018-10-12 13:56:57 -0700

    greedy sort finish

--- sorting/src/main/kotlin/com/justintullgren/hackerrank/sorting/MarkAndToys.kt
+++ sorting/src/main/kotlin/com/justintullgren/hackerrank/sorting/MarkAndToys.kt
@@ -6,6 +6,14 @@ typealias Money = Int
 /**
  * @see <a href="https://www.hackerrank.com/challenges/mark-and-toys/problem">Problem</a>
  */
-fun maximumToys(toyPrices: Array<Money>, totalMoneyToSpend: Money): MaxToys {
-    TODO()
-}
+fun maximumToys(toyPrices: Array<Money>, totalMoneyToSpend: Money): MaxToys =
+        toyPrices.sortedArray().fold(Pair(totalMoneyToSpend, 0)) { pair, toyPrice ->
+            if (pair.first >= toyPrice) {
+                val remainingMoneyToSpend = pair.first - toyPrice
+                val totalToysBought = pair.second + 1
+                Pair(remainingMoneyToSpend, totalToysBought)
+            } else {
+                pair
+            }
+
+        }.second
